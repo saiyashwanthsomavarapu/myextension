8b7b7625e411d9d0cad8edec524b26cb5841cdc1
Sai Yaswanth Somavarapu <saikkdyashwanth@gmail.com>
2024-10-22 20:52:27 +0530

    validation

--- src/webview-ui/Sidebar.tsx
+++ src/webview-ui/Sidebar.tsx
@@ -13,6 +13,7 @@ import {
     makeStyles,
     Button,
     Input,
+    Spinner,
 } from "@fluentui/react-components";
 import "./main.css";
 import { SelectBox } from "./components/SelectBox";
@@ -31,6 +32,9 @@ const styles = makeStyles({
     table: {
         marginTop: "20px",
     },
+    spinner: {
+        margin: "20px auto", // Center the spinner
+    },
 });
 
 function Sidebar() {
@@ -45,6 +49,8 @@ function Sidebar() {
     const [apiEndpoints, setApiEndpoints] = useState<{ [key: string]: string }>(
         {}
     );
+    const [loading, setLoading] = useState<boolean>(false);
+
     const style = styles();
     const rootStyle = rootStyles();
     const primaryColor = getComputedStyle(document.body).getPropertyValue(
@@ -57,18 +63,20 @@ function Sidebar() {
     useEffect(() => {
         window.addEventListener("message", (event) => {
             console.log("transformation:", event.data);
-            const transferedData = event.data;
-            if (transferedData.command === "sendData") {
-                setMetricsData(transferedData.payload.mertics);
+            const { command, payload } = event.data;
+            if (command === "sendData") {
+                setMetricsData(payload.mertics);
             }
-            if (transferedData.command === "services") {
-                setApiEndpoints(transferedData.payload.apiEndpoints);
-                setYmlData(transferedData.payload.services);
+            if (command === "services") {
+                setApiEndpoints(payload.apiEndpoints);
+                setYmlData(payload.services);
             }
+            setLoading(false);
         });
     }, []);
 
     const handleSubmit = () => {
+        setLoading(true);
         if (selectService === "dynatrace") {
             window.vscode.postMessage({
                 command: "fetchApiData",
@@ -80,6 +88,7 @@ function Sidebar() {
                     }
                 },
             });
+            setSelectedOption('');
         }
         if (selectService === "balzemeter") {
             window.vscode.postMessage({
@@ -90,8 +99,20 @@ function Sidebar() {
                     queryString: balzemeterValue,
                 },
             });
+            setBalzemeterValue({ projectId: '', workspaceId: '' });
         };
+
     }
+
+    const handelSelectService = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setSelectServices(event.target.value);
+        setMetricsData([]);
+    }
+
+    const isBalzemeterValid = Object.values(balzemeterValue).every(value => value.trim() !== '');
+    const isDynatraceValid = selectedOption.trim() !== '';
+
+    const isSubmitDisabled = selectService === "balzemeter" ? !isBalzemeterValid : selectService === "dynatrace" ? !isDynatraceValid : true;
 
     return (
         <div className={style.root}>
@@ -101,7 +122,7 @@ function Sidebar() {
                     label: key,
                     value: key,
                 }))}
-                onChange={(event) => setSelectServices(event.target.value)}
+                onChange={handelSelectService}
             />
             {selectService === "dynatrace" && (
                 <SelectBox
@@ -120,6 +141,7 @@ function Sidebar() {
                             <Input
                                 key={input}
                                 placeholder={input}
+                                name={input}
                                 onChange={(event) =>
                                     setBalzemeterValue({
                                         ...balzemeterValue,
@@ -130,9 +152,10 @@ function Sidebar() {
                         </div>
                     </div>
                 ))}
-            <Button className={style.btn} appearance="primary" onClick={handleSubmit}>
+            <Button className={style.btn} appearance="primary" disabled={isSubmitDisabled} onClick={handleSubmit}>
                 Submit
             </Button>
+            {loading && <Spinner className={style.spinner} size="large" />}
             {metricsData.length > 0 && <Table
                 className={style.table}
                 arial-label="Default table"
